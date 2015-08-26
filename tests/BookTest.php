2e909c304021fc1e923691e4ffec4321f672ec12
Mike Chastain & Casey Heitz <student@epicodus.com>
2015-08-26 10:02:48 -0700

    Add initial files

---
+++ tests/BookTest.php
@@ -0,0 +1,34 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Author.php";
+    require_once "src/Book.php";
+
+    $server = 'mysql:host=localhost;dbname=library_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class BookTest extends PHPUnit_Framework_TestCase
+    {
+
+        protected function tearDown()
+        {
+            Book::deleteAll();
+        }
+
+        function testGetTitle()
+        {
+            $title = "Anathem";
+            $test_book = new Book($title);
+
+            $result = $test_book->getTitle();
+
+            $this->assertEquals($title, $result);    
+        }
+    }
+
+ ?>
